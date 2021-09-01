a9c42da24f1f38549997e0b159b21e36f9818a3a
sekaido17 <hanetarou01@gmail.com>
2021-09-02 02:10:17 +0900

    いけますか？

--- Python/Auto_zoom_start.py
+++ Python/Auto_zoom_start.py
@@ -2,7 +2,7 @@ from os import lseek
 
 import subprocess #サブプロセスを実行するライブラリ→コマンドを実行する際に上書きしないようにプロセスを分ける
 
-meetingID = 97256163501#meetingID指定してください
+meetingID = 312321#meetingID指定してください
 
 meetingPass = "282849"#meetingpwdを指定して下さい。
 
