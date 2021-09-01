0d0a5eb54cefae6714b90527c2406c9129c278af
Mianogithub <channel_check@yahoo.co.jp>
2021-09-02 00:08:18 +0900

    画像認識(一部変更)

--- Python/画像認識/image.py
+++ Python/画像認識/image.py
@@ -13,10 +13,23 @@ tools = pyocr.get_available_tools()
 tool = tools[0]
 
 # OCR対象の画像ファイルを読み込む
-img = Image.open("test.png")
+img = Image.open("zoomImage.png")
+img_rgb = img.convert("RGB")
+pixels = img_rgb.load()
 
+# 画像を加工
+def Recolor():
+    colormin = 80
+    bluemin = 225
+    for j in range(img_rgb.size[1]):
+        for i in range(img_rgb.size[0]):
+            if (pixels[i, j][0] > colormin and pixels[i, j][1] > colormin and pixels[i, j][2] > colormin):
+                pixels[i, j] = (255, 255, 255)
+            if (pixels[i, j][2] > bluemin):
+                pixels[i, j] = (255, 255, 255)
+Recolor()
 # 画像から文字を読み込む
 builder = pyocr.builders.TextBuilder(tesseract_layout=6)
-text = tool.image_to_string(img, lang="jpn", builder=builder)
+text = tool.image_to_string(img_rgb, lang="jpn", builder=builder)
 
 print(text)
